76efa749d7d10d1d0b283bf365b9cbc6c821ca64
Andrew Do <andrew.do1613@gmail.com>
2020-12-02 00:03:24 -0800

    Refine client, use Select from react

--- wsig-client/src/Home.js
+++ wsig-client/src/Home.js
@@ -1,37 +1,36 @@
 import React from 'react'
-import CountryOptions from './CountryOptions'
-import AirportOptions from './AirportOptions'
+import Select from 'react-select'
 
-const temperatureEnum = ({
-    "none"      :   0,
-    "freezing"  :   1,
-    "cold"      :   2,
-    "cool"      :   3,
-    "warm"      :   4,
-    "hot"       :   5
-});
+const temperatureOptions = [
+    { label: "None",        value: 0 },
+    { label: "Freezing",    value: 1 },
+    { label: "Cold",        value: 2 },
+    { label: "Cool",        value: 3 },
+    { label: "Warm",        value: 4 },
+    { label: "Hot",         value: 5 }
+];
 
-const weatherEnum = ({
-    "none"      :   0,
-    "snowy"     :   1,
-    "rainy"     :   2,
-    "cloudy"    :   3,
-    "sunny"     :   4
-});
+const weatherOptions = [
+    { label: "None",        value: 0 },
+    { label: "Snowy",       value: 1 },
+    { label: "Rainy",       value: 2 },
+    { label: "Cloudy",      value: 3 },
+    { label: "Sunny",       value: 4 }
+];
 
-Object.freeze(temperatureEnum);
-Object.freeze(weatherEnum);
+Object.freeze(temperatureOptions);
+Object.freeze(weatherOptions);
 
 class Home extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            departureCountry: "",
-            departure: "",
-            destinationCountry: "",
-            destination: "",
-            preferredTemperature: temperatureEnum.none,
-            preferredWeather: weatherEnum.none,
+            departureCountry: null,
+            departure: null,
+            destinationCountry: null,
+            destination: null,
+            preferredTemperature: null,
+            preferredWeather: null,
             countryList: [],
             departureAirports: [],
             destinationAirports : []
@@ -51,7 +50,6 @@ class Home extends React.Component {
     }
 
     async getCountryList() {
-        console.log(process.env)
         try {
             let response = await fetch("http://localhost:5000/countries", {
                 "method": "GET",
@@ -81,82 +79,86 @@ class Home extends React.Component {
         //...
     }
 
-    departureCountryChange(event) {
+    async departureCountryChange(option) {
         this.setState({
-            departureCountry: event.target.value,
+            departureCountry: option,
+            departure: null
         })
-        this.getAirportList(event.target.value).then((airportList) => {
+        
+        this.getAirportList(option.value).then((airportList) => {
             this.setState({departureAirports: airportList})
         })
     }
 
-    departureChange(event) {
-        this.setState({departure: event.target.value})
+    departureChange(option) {
+        this.setState({departure: option})
     }
 
-    async destinationCountryChange(event) {
+    async destinationCountryChange(option) {
         this.setState({
-            destinationCountry: event.target.value,
+            destinationCountry: option,
+            destination: null
         })
-        this.getAirportList(event.target.value).then((airportList) => {
+        this.getAirportList(option.value).then((airportList) => {
             this.setState({destinationAirports: airportList})
         })
     }
 
-    destinationChange(event) {
-        this.setState({destination: event.value})
+    destinationChange(option) {
+        this.setState({destination: option})
     }
 
-    temperatureChange(event) {
-        this.setState({preferredTemperature: event.value})
+    temperatureChange(option) {
+        this.setState({preferredTemperature: option})
     }
 
-    weatherChange(event) {
-        this.setState({preferredWeather: event.value})
+    weatherChange(option) {
+        this.setState({preferredWeather: option})
     }
 
 
     render() {  
+        const countryListToOptions = function(country) {
+            return { 
+                label: (country.name + "\t\u2014\t" + country.alpha3Code),  
+                value: country.alpha2Code 
+            };
+        }
+
+        const airportListToOptions = function(airport) {
+            return {
+                label: airport.name + "\t\u2014\t" + airport.iata_code,
+                value: airport.iata_code
+            }
+        }
+
         var element = (
             <div className="home-form">
                 <form onSubmit={this.handleSubmit}>
                     <table><tbody>
                         <tr>
                             <td><label>Departure Country</label></td>
-                            <td><CountryOptions stateVar={this.state.departureCountry} changeCB={this.departureCountryChange} countryList={this.state.countryList}></CountryOptions></td>
+                            <td><Select options={this.state.countryList.map(countryListToOptions)} value={this.state.departureCountry} onChange={this.departureCountryChange}/></td>
                         </tr>
                         <tr>
                             <td><label>Departure Airport</label></td>
-                            <td><AirportOptions stateVar={this.state.departure} changeCB={this.departureChange} airportList={this.state.departureAirports}></AirportOptions></td>
+                            <td><Select options={this.state.departureAirports.map(airportListToOptions)} value={this.state.departure} onChange={this.departureChange}/></td>
                         </tr>
                         <tr>
                             <td><label>Destination Country</label></td>
-                            <td><CountryOptions stateVar={this.state.destinationCountry} changeCB={this.destinationCountryChange} countryList={this.state.countryList}></CountryOptions></td>
+                            <td><Select options={this.state.countryList.map(countryListToOptions)} value={this.state.destinationCountry} onChange={this.destinationCountryChange}/></td>
                         </tr>
                         <tr>
                             <td><label>Destination Airport</label></td>
-                            <td><AirportOptions stateVar={this.state.destination} changeCB={this.destinationChange} airportList={this.state.destinationAirports}></AirportOptions></td>
+                            <td><Select options={this.state.destinationAirports.map(airportListToOptions)} value={this.state.destination} onChange={this.destinationChange}/></td>
                         </tr>
                         <tr>
                             <td><label>Preferred Temperature</label></td>
-                            <td><select value={this.state.preferredTemperature} onChange={this.temperatureChange}>
-                                <option value={temperatureEnum.none}>None</option>
-                                <option value={temperatureEnum.freezing}>Freezing</option>
-                                <option value={temperatureEnum.cold}>Cold</option>
-                                <option value={temperatureEnum.cool}>Cool</option>
-                                <option value={temperatureEnum.warm}>Warm</option>
-                                <option value={temperatureEnum.hot}>Hot</option>
-                            </select></td>
+                            <td><Select options={temperatureOptions} value={this.state.preferredTemperature} onChange={this.temperatureChange}/></td>
                         </tr>
                         <tr>
                             <td><label>Preferred Weather</label></td>
-                            <td><select value={this.state.preferredWeather} onChange={this.weatherChange}>
-                                <option value={weatherEnum.none}>None</option>
-                                <option value={weatherEnum.snowy}>Snowy</option>
-                                <option value={weatherEnum.rainy}>Rainy</option>
-                                <option value={weatherEnum.cloudy}>Cloudy</option>
-                                <option value={weatherEnum.sunny}>Sunny</option>
-                            </select></td>
+                            <td><Select options={weatherOptions} value={this.state.preferredWeather} onChange={this.weatherChange}/></td>
                         </tr>
                     </tbody></table>
                 </form>

6d42d2146ddd7e4f260f6cc0d827f617d3f743a5
Andrew Do <andrew.do1613@gmail.com>
2020-12-01 21:27:26 -0800

    add background images

--- wsig-client/src/index.js
+++ wsig-client/src/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './Home';
-import './Home.css'
+import './stylesheets/Home.css'
+import BackgroundSlider from 'react-background-slider'
 import reportWebVitals from './reportWebVitals';
+
+import image1 from './images/1.jpg'
+import image2 from './images/2.jpg'
+import image3 from './images/3.jpg'
+import image4 from './images/4.jpg'
+import image5 from './images/5.jpg'
+import image6 from './images/6.jpg'
+import image7 from './images/7.jpg'
+import image8 from './images/8.jpg'
+import image9 from './images/9.jpg'
+import image10 from './images/10.jpg'
+import image11 from './images/11.jpg'
+import image12 from './images/12.jpg'
 
 ReactDOM.render(
   <React.StrictMode>
+    <BackgroundSlider 
+      images={[
+        image1,
+        image2,
+        image3,
+        image4,
+        image5,
+        image6,
+        image7,
+        image8,
+        image9,
+        image10,
+        image11,
+        image12,
+      ]}
+      duration={10}
+      transition={2}
+    />
     <Home />
   </React.StrictMode>,
   document.getElementById('root')
